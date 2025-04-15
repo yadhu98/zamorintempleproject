dbb1f5e596614c2de07c68b295c114b0ae4ebea4
yadhu98 <yadhu98@gmail.com>
2025-04-15 22:35:13 +0530

    updates

--- src/App.js
+++ src/App.js
@@ -7,109 +7,110 @@ import Manager from './images/Manager.jpg'
 import L from 'leaflet';
 import 'leaflet/dist/leaflet.css';
 import Slideshow from './ImageSlider';
+import data from './templeData.json'
 
 
-const data = [
-  {
-    district: 'Kozhikode',
-    temples: [
-      {
-        name: 'Sree Tali Devaswom (Siva & Krishna)',
-        details: 'Address: Tali, Kozhikode-2, Contact: 0495 2703610',
-        coordinates: { lat: 11.248147944046364, lng: 75.787605411904 },
-        slides : [
-          'https://www.citybit.in/wp-content/uploads/2023/10/Tali-Shiva-Temple.jpg',
-          'https://dynamic-media-cdn.tripadvisor.com/media/photo-o/11/22/cb/17/inside-tali-temple.jpg?w=700&h=400&s=1',
-          'https://www.shutterstock.com/image-photo/hindu-temple-tali-shiva-known-600w-2430677279.jpg'
-        ]
-      },
-      {
-        name: 'Sree Ramaswami Temple, (Sreerama)',
-        details: 'Address: Tali Devaswom, Tali, Kozhikode-2, Contact: 0495 2703610',
-        coordinates: { lat: 11.248832823384667, lng: 75.78693262786435 },
-        slides : [
-          'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQF7Jkm_r4VFyz9rsy20YP9ojMq7So_ZbEJAA&s',
-          'https://content3.jdmagicbox.com/comp/kozhikode/j6/0495px495.x495.220319002104.v8j6/catalogue/tali-temple-pond-vadakara-beach-kozhikode-temples-GU8KwBGM8i.jpg',
-        ]
-      },
-      {
-        name: 'Sree Valayanad Devaswom, (Devi)',
-        details: 'Address: P.O. Kommeri, Kozhikode - 7, Contact: 0495 2741083',
-        coordinates: { lat: 11.24397061799881, lng: 75.80394439470938 },
-        slides : [
-          'https://media.holidify.com/images/cmsuploads/compressed/gallery-images-03-1_20241203173837.jpg',
-          'https://s3.ap-south-1.amazonaws.com/esapp/evmpic/1548926275184.jpg',
-          'https://media1.thrillophilia.com/filestore/ycip9bow7p7ei8a0q8hv3r5rjy3t_gallery-images-07.jpg?w=400&dpr=2'
-        ]
-      },
-    ]
-  },
-  {
-    district: 'Malappuram',
-    temples: [
-      {
-        name: 'Sree Thirunavaya Devaswom, (Navamukundan),',
-        details: 'Address: P.O. Thirunavaya, Tirur,Malappuram Contact: 0494 2603747)',
-        coordinates: { lat: 10.863878637593611, lng: 75.98167325237682 },
-        slides : [
-          'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRPxbK3zXCsFWjxXeUnv3BgVXBh67n8AJcUYw&s',
-          'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSFYSTZ-uazKHQiodIHHPkSb9aRxHbkj4yCag&s',
-          'https://i.ytimg.com/vi/52qkqyAXaBI/maxresdefault.jpg'
-        ]
-      },
-      {
-        name: 'Sree Thriprangode Devaswom, (Siva),',
-        details: 'Address: P.O. Thriprangode, Tirur, Malappuram, Contact: 0494 2566046',
-        coordinates: { lat: 10.855840083961287, lng: 75.94775633888423 },
-        slides : [
-          'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSm3t4JrQCeWbPq_tqxmGYa0-E5yIJ-q8pmLA&s',
-          'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQNG_xYJsY_TGUKhfg6Cz326tNdE6Z9lVjleg&s',
-          'https://lightuptemples.com/wp-content/uploads/temple/profile_image/malappuram-sri-triprangode-shiva-temple-kerala.jpg'
-        ]
-      },
-      {
-        name: 'Sree Alathiyur Perumthrikovil Devaswom (Hanuman kavu), (Sreerama),',
-        details: 'Address: P.O. Poyilisseri, Tirur, Malappuram., Contact: 0494 2430666',
-        coordinates: { lat: 10.872272578183443, lng: 75.93921531375074 },
-        slides : [
-          'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRoiiwhr_jeeIdt5mEZOIIuo77m1uLfsJSlmA&s',
-          'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSrYnQuO-ynpVnhA19_gv1mn1svHnEWvhuEbA&s',
-          'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRxhsc_9NbMcnljj2ZiTy1AUkO7kr55aAPCOA&s'
-        ]
-      },
-    ]
-  },
-  {
-    district: 'Palakkad',
-    temples: [
-      {
-        name: 'Sree Panamanna Sankaranarayanaswami Temple',
-        details: 'Address: Sree Panamanna Devaswom,Ambalavattom, P.O. Panamanna, Ottappalam, Palakkad., Contact: 0466 2242666',
-        coordinates: { lat: 10.804542123558923, lng: 76.35485975184109 },
-        slides : [
-          'https://lh4.googleusercontent.com/proxy/rWwMd7mkxKKbGQ3KV5u22bRBOQfOUm3gqWXe1KGTc0oXRCDACpmujtl5-_HiYmle0c7irVzrVciDck8m65eix6agKj8M4k9simfzepQHnpIty-Rwu4uaH0sL',
-          'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQLOTFOv1ZTUQ5J5LlHb9LR9V_t-KD5OpgWgQ&s',
-        ]
-      },
-      {
-        name: 'Sree Kooradi Siva Temple',
-        details: 'Address: Sree Panamanna Devaswom, Panamanna, Ottapalam,Palakkad., Contact: 0466 2242666',
-        coordinates: { lat: 10.800971607281141, lng: 76.35727363111587 },
-        slides : [
-          'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTfqw13Zt_YvmSevcndtA8Dhz62OlI8cPiQ8w&s',
-        ]
-      },
-      {
-        name: 'Sree Pattarkonam Siva Temple',
-        details: 'Address: Sree Panamanna Devaswom, Panamanna,Ottapalam, Palakkad, Contact: 0466 2242666',
-        coordinates: { lat: 10.822430460489567, lng: 76.35498735335702 },
-        slides : [
-          'https://content.jdmagicbox.com/v2/comp/palakkad/k5/9999px491.x491.180223213012.p7k5/catalogue/pattarkonam-shiva-temple-palakkad-temples-gd3g5tie39-250.jpg',
-        ]
-      },
-    ]
-  }
-];
+// const data = [
+//   {
+//     district: 'Kozhikode',
+//     temples: [
+//       {
+//         name: 'Sree Tali Devaswom (Siva & Krishna)',
+//         details: 'Address: Tali, Kozhikode-2, Contact: 0495 2703610',
+//         coordinates: { lat: 11.248147944046364, lng: 75.787605411904 },
+//         slides : [
+//           'https://www.citybit.in/wp-content/uploads/2023/10/Tali-Shiva-Temple.jpg',
+//           'https://dynamic-media-cdn.tripadvisor.com/media/photo-o/11/22/cb/17/inside-tali-temple.jpg?w=700&h=400&s=1',
+//           'https://www.shutterstock.com/image-photo/hindu-temple-tali-shiva-known-600w-2430677279.jpg'
+//         ]
+//       },
+//       {
+//         name: 'Sree Ramaswami Temple, (Sreerama)',
+//         details: 'Address: Tali Devaswom, Tali, Kozhikode-2, Contact: 0495 2703610',
+//         coordinates: { lat: 11.248832823384667, lng: 75.78693262786435 },
+//         slides : [
+//           'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQF7Jkm_r4VFyz9rsy20YP9ojMq7So_ZbEJAA&s',
+//           'https://content3.jdmagicbox.com/comp/kozhikode/j6/0495px495.x495.220319002104.v8j6/catalogue/tali-temple-pond-vadakara-beach-kozhikode-temples-GU8KwBGM8i.jpg',
+//         ]
+//       },
+//       {
+//         name: 'Sree Valayanad Devaswom, (Devi)',
+//         details: 'Address: P.O. Kommeri, Kozhikode - 7, Contact: 0495 2741083',
+//         coordinates: { lat: 11.24397061799881, lng: 75.80394439470938 },
+//         slides : [
+//           'https://media.holidify.com/images/cmsuploads/compressed/gallery-images-03-1_20241203173837.jpg',
+//           'https://s3.ap-south-1.amazonaws.com/esapp/evmpic/1548926275184.jpg',
+//           'https://media1.thrillophilia.com/filestore/ycip9bow7p7ei8a0q8hv3r5rjy3t_gallery-images-07.jpg?w=400&dpr=2'
+//         ]
+//       },
+//     ]
+//   },
+//   {
+//     district: 'Malappuram',
+//     temples: [
+//       {
+//         name: 'Sree Thirunavaya Devaswom, (Navamukundan),',
+//         details: 'Address: P.O. Thirunavaya, Tirur,Malappuram Contact: 0494 2603747)',
+//         coordinates: { lat: 10.863878637593611, lng: 75.98167325237682 },
+//         slides : [
+//           'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRPxbK3zXCsFWjxXeUnv3BgVXBh67n8AJcUYw&s',
+//           'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSFYSTZ-uazKHQiodIHHPkSb9aRxHbkj4yCag&s',
+//           'https://i.ytimg.com/vi/52qkqyAXaBI/maxresdefault.jpg'
+//         ]
+//       },
+//       {
+//         name: 'Sree Thriprangode Devaswom, (Siva),',
+//         details: 'Address: P.O. Thriprangode, Tirur, Malappuram, Contact: 0494 2566046',
+//         coordinates: { lat: 10.855840083961287, lng: 75.94775633888423 },
+//         slides : [
+//           'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSm3t4JrQCeWbPq_tqxmGYa0-E5yIJ-q8pmLA&s',
+//           'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQNG_xYJsY_TGUKhfg6Cz326tNdE6Z9lVjleg&s',
+//           'https://lightuptemples.com/wp-content/uploads/temple/profile_image/malappuram-sri-triprangode-shiva-temple-kerala.jpg'
+//         ]
+//       },
+//       {
+//         name: 'Sree Alathiyur Perumthrikovil Devaswom (Hanuman kavu), (Sreerama),',
+//         details: 'Address: P.O. Poyilisseri, Tirur, Malappuram., Contact: 0494 2430666',
+//         coordinates: { lat: 10.872272578183443, lng: 75.93921531375074 },
+//         slides : [
+//           'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRoiiwhr_jeeIdt5mEZOIIuo77m1uLfsJSlmA&s',
+//           'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSrYnQuO-ynpVnhA19_gv1mn1svHnEWvhuEbA&s',
+//           'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRxhsc_9NbMcnljj2ZiTy1AUkO7kr55aAPCOA&s'
+//         ]
+//       },
+//     ]
+//   },
+//   {
+//     district: 'Palakkad',
+//     temples: [
+//       {
+//         name: 'Sree Panamanna Sankaranarayanaswami Temple',
+//         details: 'Address: Sree Panamanna Devaswom,Ambalavattom, P.O. Panamanna, Ottappalam, Palakkad., Contact: 0466 2242666',
+//         coordinates: { lat: 10.804542123558923, lng: 76.35485975184109 },
+//         slides : [
+//           'https://lh4.googleusercontent.com/proxy/rWwMd7mkxKKbGQ3KV5u22bRBOQfOUm3gqWXe1KGTc0oXRCDACpmujtl5-_HiYmle0c7irVzrVciDck8m65eix6agKj8M4k9simfzepQHnpIty-Rwu4uaH0sL',
+//           'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQLOTFOv1ZTUQ5J5LlHb9LR9V_t-KD5OpgWgQ&s',
+//         ]
+//       },
+//       {
+//         name: 'Sree Kooradi Siva Temple',
+//         details: 'Address: Sree Panamanna Devaswom, Panamanna, Ottapalam,Palakkad., Contact: 0466 2242666',
+//         coordinates: { lat: 10.800971607281141, lng: 76.35727363111587 },
+//         slides : [
+//           'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTfqw13Zt_YvmSevcndtA8Dhz62OlI8cPiQ8w&s',
+//         ]
+//       },
+//       {
+//         name: 'Sree Pattarkonam Siva Temple',
+//         details: 'Address: Sree Panamanna Devaswom, Panamanna,Ottapalam, Palakkad, Contact: 0466 2242666',
+//         coordinates: { lat: 10.822430460489567, lng: 76.35498735335702 },
+//         slides : [
+//           'https://content.jdmagicbox.com/v2/comp/palakkad/k5/9999px491.x491.180223213012.p7k5/catalogue/pattarkonam-shiva-temple-palakkad-temples-gd3g5tie39-250.jpg',
+//         ]
+//       },
+//     ]
+//   }
+// ];
 
 const truste = [
   {
